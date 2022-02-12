1d19b63810832ba923bbffb088c748100ee9872f
Clavianus Juneardo <clavianus.juneardo@traveloka.com>
2022-02-12 14:27:00 +0700

    add golang SONAR_TOKEN key
    Signed-off-by: Clavianus Juneardo <clavianus.juneardo@traveloka.com>

--- github_variables.tf
+++ github_variables.tf
@@ -2,3 +2,8 @@ variable "BJORA_PROJECT_TERRAFORM_SONAR_TOKEN" {
   type      = string
   sensitive = true
 }
+
+variable "BJORA_PROJECT_GOLANG_SONAR_TOKEN" {
+  type      = string
+  sensitive = true
+}
