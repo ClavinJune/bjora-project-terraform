1d4bc2b53e8ab5664c3dfa694ff701cafaf56c3a
Clavianus Juneardo <clavianus.juneardo@traveloka.com>
2022-02-12 14:09:13 +0700

    add backend repo
    Signed-off-by: Clavianus Juneardo <clavianus.juneardo@traveloka.com>

--- github_branch_default.tf
+++ github_branch_default.tf
@@ -7,3 +7,8 @@ resource "github_branch_default" "bjora-project-terraform" {
   repository = github_repository.bjora-project-terraform.name
   branch     = "main"
 }
+
+resource "github_branch_default" "bjora-project-golang" {
+  repository = github_repository.bjora-project-golang.name
+  branch     = "main"
+}
--- github_repository.tf
+++ github_repository.tf
@@ -22,3 +22,16 @@ resource "github_repository" "bjora-project-terraform" {
   gitignore_template     = "Terraform"
   license_template       = "mit"
 }
+
+resource "github_repository" "bjora-project-golang" {
+  name        = "bjora-project-golang"
+  description = "bjora project golang"
+
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+  allow_auto_merge       = false
+  delete_branch_on_merge = true
+  auto_init              = true
+  gitignore_template     = "Go"
+  license_template       = "mit"
+}
