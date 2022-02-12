f46683bcac5f8e169329addf12c791e18953beea
Clavianus Juneardo <clavianus.juneardo@traveloka.com>
2022-02-12 14:28:54 +0700

    add golang SONAR_TOKEN key (#6)
    Signed-off-by: Clavianus Juneardo <clavianus.juneardo@traveloka.com>

--- github_actions_secret.tf
+++ github_actions_secret.tf
@@ -3,3 +3,9 @@ resource "github_actions_secret" "bjora-project-terraform-sonar-token" {
   secret_name     = "SONAR_TOKEN"
   plaintext_value = var.BJORA_PROJECT_TERRAFORM_SONAR_TOKEN
 }
+
+resource "github_actions_secret" "bjora-project-golang-sonar-token" {
+  repository      = github_repository.bjora-project-golang.name
+  secret_name     = "SONAR_TOKEN"
+  plaintext_value = var.BJORA_PROJECT_GOLANG_SONAR_TOKEN
+}
