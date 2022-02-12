6c66992f932460a415255ef58e9016a9678dfe96
Clavianus Juneardo <clavianus.juneardo@traveloka.com>
2022-02-12 13:12:35 +0700

    testing
    Signed-off-by: Clavianus Juneardo <clavianus.juneardo@traveloka.com>

---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "~> 1.1.0"
+
+  cloud {
+      organization = "bjora"
+      workspaces {
+          name = "bjora-project-terraform"
+      }
+  }
+
+  required_providers {
+    github = {
+      version = "~> 4.0"
+      source  = "integrations/github"
+    }
+  }
+}
