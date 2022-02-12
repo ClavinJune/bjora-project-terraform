55c6d9a89f215daf1c80661594ba7027145605d5
Clavianus Juneardo <clavianus.juneardo@traveloka.com>
2022-02-12 13:58:16 +0700

    update SONAR_TOKEN key
    Signed-off-by: Clavianus Juneardo <clavianus.juneardo@traveloka.com>

--- github_actions_secret.tf
+++ github_actions_secret.tf
@@ -1,5 +1,5 @@
 resource "github_actions_secret" "bjora-project-terraform-sonar-token" {
   repository       = github_repository.bjora-project-terraform.name
-  secret_name      = "SONAR_TOKEN"
-  plaintext_value = var.SONAR_TOKEN
+  secret_name      = "BJORA_PROJECT_TERRAFORM_SONAR_TOKEN"
+  plaintext_value = var.BJORA_PROJECT_TERRAFORM_SONAR_TOKEN
 }
