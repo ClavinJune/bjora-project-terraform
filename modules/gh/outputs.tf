f65bbefe54ff8b33d12f256c6d499606fc16f8ad
Clavianus Juneardo <clavianus.juneardo@traveloka.com>
2022-02-27 23:46:30 +0700

    removel ocal modules (#12)
    Signed-off-by: Clavianus Juneardo <clavianus.juneardo@traveloka.com>

--- modules/gh/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  description = "repository name"
-  value       = github_repository.this.name
-}
-
-output "url" {
-  description = "repository URL"
-  value       = github_repository.this.html_url
-}
